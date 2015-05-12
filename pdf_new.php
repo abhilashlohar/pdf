13f77c2c4c5ceaf55c2749aef62e2b6fae128731
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-12 14:37:17 +0530

    sfdsf

--- pdf_new.php
+++ pdf_new.php
@@ -218,8 +218,9 @@ $pdf->SetAutoPageBreak($auto_page_break, $bMargin);
 // set the starting point for the page content
 $pdf->setPageMark();
 
-
-$pdf->writeHTML($middel, true, false, true, false, '');
+$middel_page='<br/><br/><br/>';
+$middel_page.=$middel;
+$pdf->writeHTML($middel_page, true, false, true, false, '');
 
 $pdf->AddPage();
 // get the current page break margin
