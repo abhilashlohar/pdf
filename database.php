f85cba0bc4fb87f3d8eb31310e26061db294f4b8
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-12 13:24:43 +0530

    password

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "phppoets";
 $conn = mysql_connect($servername, $username, $password);
 mysql_select_db( 'quotation' );
 ?>
