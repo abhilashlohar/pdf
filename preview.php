2db0dc7703e0ee80ded212ae3776491c39552521
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-14 19:03:06 +0530

    sdf

---
+++ preview.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+
+
+
+<script type="text/javascript">
+var key = 1;
+(function(a) {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'http://52.74.43.53/growth-heacker/feed.js?key='+key;var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);}(window, document))
+</script>
+</head>
+
+<body style="margin:0;">
+<h1>hello</h1>
+preview.php
+</body>
+</html>
