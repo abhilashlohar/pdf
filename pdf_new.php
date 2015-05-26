679ffabd97284007cd19829b7ff432f84cdacf30
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-26 13:01:06 +0530

    asdsd

--- pdf_new.php
+++ pdf_new.php
@@ -85,13 +85,15 @@ $pdf->setPageMark();
 
 
 $toolcopy = '<br/><br/><br/><br/><br/><br/><span style="font-size:12;"><b> Quotation prepared for </b></span>';
+$pdf->writeHTML($toolcopy, true, false, true, false, '');
+
+$toolcopy = '<span style="font-size:13;color:rgb(0,160,227);font-weight: bold;"> <b>Fusion Tours</b></span>';
 $pdf->SetFont($fontNameBold, 'BI', 10, '', 'false');
 $pdf->writeHTML($toolcopy, true, false, true, false, '');
-$toolcopy = '<span style="font-size:13;color:rgb(0,160,227);font-weight: bold;"> <b>Fusion Tours</b></span>';
-$toolcopy .= 'hello';
-$pdf->writeHTML($toolcopy, true, false, true, false, '');
+
+
 $pdf->SetFont($fontname, 'BI', 10, '', 'false');
-$toolcopy = 'hello';
+
 $toolcopy .= '<br/><br/><br/><br/>';
 $toolcopy .= '<table cellpadding="5">';
 $toolcopy .= '<tr>
