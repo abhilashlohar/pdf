cb310f66628180d8dccadea6b6840af92c65da16
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-26 12:57:41 +0530

    sadas

--- pdf_new.php
+++ pdf_new.php
@@ -84,12 +84,14 @@ $pdf->setPageMark();
 
 
 
-$toolcopy = '<br/><br/><br/><br/><br/><br/><div><span style="font-size:12;"><b> Quotation prepared for </b></span>';
+$toolcopy = '<br/><br/><br/><br/><br/><br/><span style="font-size:12;"><b> Quotation prepared for </b></span>';
 $pdf->SetFont($fontNameBold, 'BI', 10, '', 'false');
-$toolcopy .= '<span style="font-size:13;color:rgb(0,160,227);font-weight: bold;"> <b>Fusion Tours</b></span></div>';
+$pdf->writeHTML($toolcopy, true, false, true, false, '');
+$toolcopy = '<span style="font-size:13;color:rgb(0,160,227);font-weight: bold;"> <b>Fusion Tours</b></span>';
 $toolcopy .= 'hello';
+$pdf->writeHTML($toolcopy, true, false, true, false, '');
 $pdf->SetFont($fontname, 'BI', 10, '', 'false');
-$toolcopy .= 'hello';
+$toolcopy = 'hello';
 $toolcopy .= '<br/><br/><br/><br/>';
 $toolcopy .= '<table cellpadding="5">';
 $toolcopy .= '<tr>
