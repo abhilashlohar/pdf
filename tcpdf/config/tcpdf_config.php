8454b25ba800aa6db30ff95df2bb10f3dc565e56
Abhilash Lohar <abhilashlohar01@gmail.com>
2015-05-30 12:12:29 +0530

    werwer

--- tcpdf/config/tcpdf_config.php
+++ tcpdf/config/tcpdf_config.php
@@ -145,7 +145,7 @@ if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
 	/**
 	 * header logo image width [mm]
 	 */
-	define ('PDF_HEADER_LOGO_WIDTH', 100%);
+	define ('PDF_HEADER_LOGO_WIDTH', 50);
 
 	/**
 	 *  document unit of measure [pt=point, mm=millimeter, cm=centimeter, in=inch]
